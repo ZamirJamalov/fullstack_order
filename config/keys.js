437d6a6d98a7ddf7a88b39fef8c6af551d5fa21e
zamir jamalov <zamir.jamalov@gmail.com>
2018-08-12 17:31:42 +0400

    login to system

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-	mongoURI:'mongodb://zam:zam220783@ds219532.mlab.com:19532/fullstack_order'
+	mongoURI:'mongodb://zam:zam220783@ds219532.mlab.com:19532/fullstack_order',
+	jwt:'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,37 @@
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 const User=require('../models/User')
+const keys=require('../config/keys')
 
-module.exports.login=function(req,res){
-	res.status(200).json({
-		login: {
-			email: req.body.email,
-			password: req.body.password
+module.exports.login=async function(req,res){
+	const candidate=await User.findOne({email:req.body.email})
+
+	if (candidate){
+		//check password
+		const passwordResult=bcrypt.compareSync(req.body.password, candidate.password)
+		if (passwordResult){
+			//create token
+			const token=jwt.sign({
+				email: candidate.email,
+				userId: candidate._id
+			}, keys.jwt,{expiresIn: 60*60})
+
+			res.status(200).json({
+				token:`Bearer ${token}`
+			})
+		} else {
+			//no password match
+			res.status(401).json({
+				message:'no password match'
+			})
 		}
-	})
+
+	} else {
+		//no user found error
+		res.status(404).json({
+			message:'no user found'
+		})
+	}
 }
 
 module.exports.register= async function(req,res){
